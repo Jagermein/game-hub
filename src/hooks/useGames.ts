09c1861e70878135c8d5af8bfee95252b38da6d8
Chance Norris <chancenorris@Mac.lan>
2025-03-04 12:44:21 -0800

    display genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
@@ -16,38 +14,7 @@ export interface Game {
   metacritic: number;
 }
 
-interface fetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 
-const useGames = () =>{
+const useGames = () => useData<Game>('/games');
 
-  const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState('')
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-
-    const controller = new AbortController();
-    setLoading(true)
-    apiClient.get<fetchGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch(err => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-
-  }, []);
-
-  return { games, error, isLoading }
-
-}
-
-export default useGames
+export default useGames;
