b5d6d817f4149859d3907d8d78ff5b814381a30e
Chance Norris <chancenorris@Mac.lan>
2025-05-06 11:50:10 -0700

    highlighting the choice

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,31 +1,51 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import {
+    Button,
+    HStack,
+    Image,
+    List,
+    ListItem,
+    Spinner,
+} from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onSelectGenre: (genre: Genre) => void;
+    onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+    const { data, isLoading, error } = useData<Genre>("/genres");
 
-  const { data, isLoading, error } = useData<Genre>('/genres');
+    if (error) return null;
+    if (isLoading) return <Spinner />;
 
-  if (error) return null
-  if (isLoading) return <Spinner />;
-
-  return (
-    <List>
-      {data.map((genre) => (<ListItem key={genre.id} paddingY={'5px'}>
-        <HStack>
-          <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-          <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>
-            {genre.name}
-          </Button>
-        </HStack>
-        </ListItem>))}
-    </List>
-  );
-}
+    return (
+        <List>
+            {data.map((genre) => (
+                <ListItem key={genre.id} paddingY={"5px"}>
+                    <HStack>
+                        <Image
+                            boxSize="32px"
+                            borderRadius={8}
+                            src={getCroppedImageUrl(genre.image_background)}
+                        />
+                        <Button
+                            onClick={() => onSelectGenre(genre)}
+                            fontWeight={
+                                genre.id === selectedGenre?.id ? "bold" : "norm"
+                            }
+                            fontSize="lg"
+                            variant="link"
+                        >
+                            {genre.name}
+                        </Button>
+                    </HStack>
+                </ListItem>
+            ))}
+        </List>
+    );
+};
 
 export default GenreList;
