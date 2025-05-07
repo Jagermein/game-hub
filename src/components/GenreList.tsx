4c48433cdfca4d69c6d65d1693639a922c31886b
Chance Norris <chancenorris@Mac.lan>
2025-05-07 09:10:34 -0700

    changing chakra theme

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,7 +8,7 @@ import {
     Spinner,
 } from "@chakra-ui/react";
 import useData from "../hooks/useData";
-import { Genre } from "../hooks/useGenres";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
@@ -17,7 +17,7 @@ interface Props {
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-    const { data, isLoading, error } = useData<Genre>("/genres");
+    const { data, isLoading, error } = useGenres();
 
     if (error) return null;
     if (isLoading) return <Spinner />;
